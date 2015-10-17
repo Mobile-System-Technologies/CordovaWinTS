b4dd788bdeb458633506c298efd3d48fd4f60c86
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2015-10-17 06:31:20 -0600

    update

--- scripts/home.ts
+++ scripts/home.ts
@@ -1,17 +1,31 @@
 ﻿module CordovaWinTS
 {
     export class HomeViewManager
-    { 
-        _targetDiv: string;
+    {
+        _targetDiv: HTMLElement;
         _pageUri: string;
 
+        constructor(uri?:string, targetId?:HTMLElement){
+            if (uri !== null && targetId !== null){
+                WinJS.UI.Fragments.renderCopy(uri, targetId).done(
+                    () => {
+                        console.log('Fragmento Cargado');
+                    },
+                    (error) => {
+                        console.log("El Fragmento no Pudo ser Cargado");
+                    });
+            }
+            else {
+                console.log("{}")
+            }
+        }
 
-        get targetDiv(): string
+        get targetDiv(): HTMLElement
         {
             return this._targetDiv;
         }
 
-        set targetDiv(div:string)
+        set targetDiv(div:HTMLElement)
         {
             this._targetDiv = div
         }
@@ -28,7 +42,14 @@
 
         renderFragment()
         {
-
+            this.targetDiv.innerHTML = "";
+            WinJS.UI.Fragments.renderCopy(this.pageUri, this.targetDiv).done(
+                () => {
+                    console.log('Fragmento Cargado');
+                },
+                (error) => {
+                    console.log("El Fragmento no Pudo ser Cargado");
+                });
         }
     }
 
